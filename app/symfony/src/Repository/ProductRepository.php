381ec7d896561596726e7139a78b256cb840c78e
msow <msow@kernix.com>
2020-12-04 19:25:41 +0100

    code review

---
+++ app/symfony/src/Repository/ProductRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Letter;
+use App\Entity\Product;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Letter|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Letter|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Letter[]    findAll()
+ * @method Letter[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ProductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Product::class);
+    }
+
+}
