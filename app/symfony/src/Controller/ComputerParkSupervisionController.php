f6acdcda479e76b865be481a73a148a864f073a0
msow <msow@kernix.com>
2020-12-24 01:42:19 +0100

    secirity informatique

---
+++ app/symfony/src/Controller/ComputerParkSupervisionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ComputerParkSupervisionController extends AbstractController
+{
+    /**
+     * @Route("/computer/park/supervision", name="computer_park_supervision")
+     */
+    public function index(): Response
+    {
+
+        return $this->render('computer_park_supervision/index.html.twig', [
+
+            'controller_name' => 'ComputerParkSupervisionController',
+
+        ]);
+    }
+    /**
+     * @Route("/computer/park/supervisions", name="computer_park")
+     */
+    public function pack(): Response
+    {
+
+        return $this->render('computer_park_supervision/index.html.twig', [
+
+            'controller_name' => 'ComputerParkSupervisionController',
+
+        ]);
+    }
+}
