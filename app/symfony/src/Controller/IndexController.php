76f02e29e71aef714dc07ce1da4973c40315964a
msow <msow@kernix.com>
2021-01-02 21:30:23 +0100

    add new leter

--- app/symfony/src/Controller/IndexController.php
+++ app/symfony/src/Controller/IndexController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\NewsLetter;
 use App\Entity\SlideShow;
 use App\Entity\User;
+use App\Form\NewsLetterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +16,19 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="3dtech_index")
      */
-    public function index()
+    public function index(Request $request)
     {
-
+        $newLetter = new NewsLetter();
+        $newLetterForm = $this->createForm(NewsLetterType::class, $newLetter);
+        $newLetterForm->handleRequest($request);
+        if ($newLetterForm->isSubmitted() and $newLetterForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newLetter);
+            $em->flush();
+        }
         return $this->render('home/index.html.twig', [
             'slideShow' => '',
+            'form' => $newLetterForm->createView(),
         ]);
     }
 
