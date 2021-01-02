c2b7a2a2cd415afa61c2c0f81c6c34452d14887c
msow <msow@kernix.com>
2021-01-02 22:25:41 +0100

    code review

---
+++ app/symfony/src/Controller/NewsLetterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\NewsLetter;
+use App\Form\NewsLetterType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewsLetterController extends AbstractController
+{
+    /**
+     * @Route("/news/letter", name="add_news_letter")
+     */
+    public function addNewEmail(Request $request): Response
+    {
+        $newLetter = new NewsLetter();
+        dd($request);
+        $newLetterForm = $this->createForm(NewsLetterType::class, $newLetter);
+        $newLetterForm->handleRequest($request);
+        if ($newLetterForm->isSubmitted() and $newLetterForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newLetter);
+            $em->flush();
+        }
+        return $this->redirectToRoute('3dtech_index');
+
+    }
+}
---
+++ app/symfony/src/Entity/NewsLetter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsLetterRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsLetterRepository::class)
+ * @UniqueEntity("email");
+ */
+class NewsLetter
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=80, unique=true)
+
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
