41280587f3ca2ebfc6ca6755562fd41d07404e55
msow <msow@kernix.com>
2021-01-02 14:51:42 +0100

    fix contact

--- app/symfony/config/bundles.php
+++ app/symfony/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
