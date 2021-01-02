c39c3fe4e169802f8e6bec30b3906a215fc3d65e
msow <msow@kernix.com>
2021-01-02 22:44:34 +0100

    code review

--- app/symfony/src/Controller/IndexController.php
+++ app/symfony/src/Controller/IndexController.php
@@ -20,8 +20,7 @@ class IndexController extends AbstractController
      */
     public function index(Request $request, AskQuoteManager $askQuoteManager)
     {
-        $ask = $this->getDoctrine()->getRepository(AskOfQuote::class)->find(1);
-        $askQuoteManager->sendAskQuoteMail($ask);
+
         return $this->render('home/index.html.twig', [
             'slideShow' => '',
         ]);
