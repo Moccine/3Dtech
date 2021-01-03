93e011b2a7b1919bef2c624ee136f12b62e43b9d
msow <msow@kernix.com>
2021-01-03 16:05:35 +0100

    add contact

---
+++ app/symfony/src/Entity/Contact.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\CreatedAtTrait;
+use App\Entity\Traits\IdentifiableTrait;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+
+
+    use IdentifiableTrait;
+    use CreatedAtTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="string", length=90)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     */
+    private $message;
+
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
